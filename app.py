52f106c68a1a4cb1d51003c2b900a92a478e7692
tommymmcguire <141086024+tommymmcguire@users.noreply.github.com>
2023-12-07 02:18:38 +0000

    good

--- app.py
+++ app.py
@@ -6,14 +6,26 @@ import io
 app = Flask(__name__)
 
 def overlay_image_on_tshirt(generated_image, tshirt_template_path, output_path):
-    tshirt = Image.open(tshirt_template_path)
-    generated_image = generated_image.resize((300, 300))  # Example size, adjust as needed
+    # Open the t-shirt template and convert it to RGBA if it's not already
+    tshirt = Image.open(tshirt_template_path).convert("RGBA")
+
+    # Resize the generated image
+    generated_image = generated_image.resize((150, 150))  # Example size, adjust as needed
+
+    # Ensure the generated image is in RGBA mode
+    if generated_image.mode != "RGBA":
+        generated_image = generated_image.convert("RGBA")
 
     # Example position, adjust as needed
-    position = (100, 100)  
+    position = (200, 200)  
+
+    # Paste the generated image onto the t-shirt
+    # Use the alpha channel of the generated image as the mask if it exists
     tshirt.paste(generated_image, position, generated_image)
+
     tshirt.save(output_path)
     return output_path
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -24,23 +36,26 @@ def generate_image():
     description = request.form.get('description')
 
     # Replace with actual DALL-E 2 API call
-    headers = {"Authorization": "Bearer YOUR_API_KEY"}
-    api_url = "https://api.openai.com/v1/dalle-2/generate"
+    headers = {"Authorization": "Bearer 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"}
+    api_url = "https://api.openai.com/v1/images/generations"
     response = requests.post(api_url, headers=headers, json={"prompt": description})
 
     if response.status_code == 200:
         # The response handling here depends on how DALL-E 2 returns the image.
         # You might receive a URL, binary data, etc. Adjust the following line accordingly.
-        generated_image_data = response.json()["image_data"]  # Adjust based on actual response format
+        image_urls = response.json()["data"]
+        first_image_url = image_urls[0]["url"]  # Using the first image URL
 
-        # Convert the image data to a PIL Image object
-        generated_image = Image.open(io.BytesIO(generated_image_data))
+        # Download the image from the URL (optional, if needed for processing)
+        response = requests.get(first_image_url)
+        if response.status_code == 200:
+            generated_image = Image.open(io.BytesIO(response.content))
+            output_path = 'static/output_tshirt.png'
+            tshirt_image_path = overlay_image_on_tshirt(generated_image, 'static/tshirt_template.png', output_path)
 
-        # Overlay the image onto the t-shirt
-        output_path = 'static/output_tshirt.png'
-        tshirt_image_path = overlay_image_on_tshirt(generated_image, 'static/tshirt_template.png', output_path)
-
-        return render_template('display_tshirt.html', tshirt_image=tshirt_image_path)
+            return render_template('display_tshirt.html', tshirt_image='output_tshirt.png')
+        else:
+            return jsonify({"error": "Failed to download the image"}), response.status_code
     else:
         return jsonify({"error": "Failed to generate image"}), response.status_code
 
---
+++ test_app.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import patch, mock_open
+from app import app, overlay_image_on_tshirt
+from PIL import Image
+import io
+import os
+
+class TestDalleIntegration(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    @patch('requests.post')
+    def test_generate_image(self, mock_post):
+        # Mock the response of DALL-E API
+        mock_image = Image.new('RGB', (100, 100))
+        img_byte_arr = io.BytesIO()
+        mock_image.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "data": [{"url": "https://..."}]
+        }
+
+        # Mock requests.get for image download
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.content = img_byte_arr
+
+            response = self.app.post('/generate-image', data={"description": "test"})
+            self.assertEqual(response.status_code, 200)
+            # Add more assertions here as necessary
+
+class TestImageOverlay(unittest.TestCase):
+    def test_image_overlay(self):
+        # Create a mock generated image and a mock t-shirt template
+        generated_image = Image.new('RGB', (100, 100))
+        tshirt_template = Image.new('RGB', (300, 300))
+        tshirt_template_path = 'temp_tshirt_template.png'
+        tshirt_template.save(tshirt_template_path)
+
+        output_path = 'temp_output.png'
+        overlay_image_on_tshirt(generated_image, tshirt_template_path, output_path)
+        
+        # Check if the output image exists and has the properties you expect
+        self.assertTrue(os.path.exists(output_path))
+        # Add more assertions as necessary
+
+        # Cleanup: Remove the temporary files after the test
+        os.remove(tshirt_template_path)
+        os.remove(output_path)
+
+if __name__ == '__main__':
+    unittest.main()
