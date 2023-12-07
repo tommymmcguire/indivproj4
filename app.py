f229b6df5dc7b118e08fd031c6ee3a0fbe436302
tommymmcguire <141086024+tommymmcguire@users.noreply.github.com>
2023-12-07 03:19:35 +0000

    format

--- app.py
+++ app.py
@@ -10,19 +10,20 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 def overlay_image_on_tshirt(generated_image, tshirt_template_path, output_path):
     # Open the t-shirt template and convert it to RGBA if it's not already
     tshirt = Image.open(tshirt_template_path).convert("RGBA")
 
     # Resize the generated image
-    generated_image = generated_image.resize((150, 150)) # Ex. adjust as needed
+    generated_image = generated_image.resize((150, 150))  # Ex. adjust as needed
 
     # Ensure the generated image is in RGBA mode
     if generated_image.mode != "RGBA":
         generated_image = generated_image.convert("RGBA")
 
     # Example position, adjust as needed
-    position = (200, 200)  
+    position = (200, 200)
 
     # Paste the generated image onto the t-shirt
     # Use the alpha channel of the generated image as the mask if it exists
@@ -32,15 +33,16 @@ def overlay_image_on_tshirt(generated_image, tshirt_template_path, output_path):
     return output_path
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/generate-image', methods=['POST'])
+
+@app.route("/generate-image", methods=["POST"])
 def generate_image():
-    description = request.form.get('description')
+    description = request.form.get("description")
 
-    api_key = os.getenv('OPENAI_API_KEY')
+    api_key = os.getenv("OPENAI_API_KEY")
     headers = {"Authorization": f"Bearer {api_key}"}
     api_url = "https://api.openai.com/v1/images/generations"
     response = requests.post(api_url, headers=headers, json={"prompt": description})
@@ -54,15 +56,22 @@ def generate_image():
         response = requests.get(first_image_url)
         if response.status_code == 200:
             generated_image = Image.open(io.BytesIO(response.content))
-            output_path = 'static/output_tshirt.png'
-            tshirt_image_path = overlay_image_on_tshirt(generated_image, 'static/tshirt_template.png', output_path)
+            output_path = "static/output_tshirt.png"
+            tshirt_image_path = overlay_image_on_tshirt(
+                generated_image, "static/tshirt_template.png", output_path
+            )
 
-            return render_template('display_tshirt.html', tshirt_image='output_tshirt.png')
+            return render_template(
+                "display_tshirt.html", tshirt_image="output_tshirt.png"
+            )
         else:
-            return jsonify({"error": "Failed to download the image"}), response.status_code
+            return (
+                jsonify({"error": "Failed to download the image"}),
+                response.status_code,
+            )
     else:
         return jsonify({"error": "Failed to generate image"}), response.status_code
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
